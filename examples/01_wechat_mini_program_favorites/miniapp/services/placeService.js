3716571fa79019e4f82cc17f7ba00c86213d0982
之道工作室 <126126815+zhidao-studio@users.noreply.github.com>
2026-07-05 21:50:59 +0800

    删除旧小程序地点服务

--- examples/01_wechat_mini_program_favorites/miniapp/services/placeService.js
+++
@@ -1,4 +0,0 @@
-const { places } = require('../mock/places')
-function getPlaces() { return places }
-function getPlaceById(id) { return places.find(item => item.id === id) }
-module.exports = { getPlaces, getPlaceById }
