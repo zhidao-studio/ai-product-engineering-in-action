f348e62ab246e1815e89f6c7365412ca9cd33035
之道工作室 <126126815+zhidao-studio@users.noreply.github.com>
2026-07-06 22:26:13 +0800

    新增后端模板业务异常

---
+++ 工程模板/后端模板工程/src/main/java/com/example/template/exception/BusinessException.java
@@ -0,0 +1,17 @@
+package com.example.template.exception;
+
+import com.example.template.common.ErrorCode;
+
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
