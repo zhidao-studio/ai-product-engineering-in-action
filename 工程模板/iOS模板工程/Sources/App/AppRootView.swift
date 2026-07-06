ea3abfcd5f4fd7f27c8eb976d07fe2b04ff00f3c
之道工作室 <126126815+zhidao-studio@users.noreply.github.com>
2026-07-06 23:39:38 +0800

    新增 iOS 根视图

---
+++ 工程模板/iOS模板工程/Sources/App/AppRootView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+public struct AppRootView: View {
+    public init() {}
+
+    public var body: some View {
+        NavigationStack {
+            HomeView()
+        }
+    }
+}
