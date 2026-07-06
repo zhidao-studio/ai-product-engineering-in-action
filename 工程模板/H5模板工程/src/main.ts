37e2e5c6b983eac716947eb2f32fd03c1422e79a
之道工作室 <126126815+zhidao-studio@users.noreply.github.com>
2026-07-06 23:32:38 +0800

    新增 H5 模板 main.ts

---
+++ 工程模板/H5模板工程/src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+import App from './App.vue'
+import router from './router'
+import './styles/design-tokens.css'
+import './styles/global.css'
+
+createApp(App).use(createPinia()).use(router).use(Vant).mount('#app')
