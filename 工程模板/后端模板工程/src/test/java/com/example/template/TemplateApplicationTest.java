39fe9d8544b0a0af66e98dd224073f8e4f947d24
之道工作室 <126126815+zhidao-studio@users.noreply.github.com>
2026-07-06 22:51:04 +0800

    新增后端模板基础测试

---
+++ 工程模板/后端模板工程/src/test/java/com/example/template/TemplateApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.template;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TemplateApplicationTest {
+
+    @Test
+    void basicTest() {
+        assertTrue(true);
+    }
+}
