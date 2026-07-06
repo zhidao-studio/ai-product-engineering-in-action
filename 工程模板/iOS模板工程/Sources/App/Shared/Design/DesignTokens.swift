fd06095912c06c2fb69981e185b0c8d6b3b39972
之道工作室 <126126815+zhidao-studio@users.noreply.github.com>
2026-07-06 23:41:59 +0800

    新增 iOS 设计变量

---
+++ 工程模板/iOS模板工程/Sources/App/Shared/Design/DesignTokens.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+public enum DesignTokens {
+    public static let spacingSmall: CGFloat = 8
+    public static let spacingMedium: CGFloat = 16
+    public static let spacingLarge: CGFloat = 24
+    public static let cornerRadius: CGFloat = 12
+}
