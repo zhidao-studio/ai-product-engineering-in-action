88c131576ebe0192ed4a6c4978d14d55a8af59ed
之道工作室 <126126815+zhidao-studio@users.noreply.github.com>
2026-07-07 21:35:42 +0800

    统一系统用户 ID 为 32 位字符串

--- 工程模板/后端模板工程/src/main/java/com/example/template/common/DataFlag.java
+++ 工程模板/后端模板工程/src/main/java/com/example/template/common/DataFlag.java
@@ -6,7 +6,7 @@ public final class DataFlag {
     public static final int INVALID = 0;
     public static final int DELETED = 1;
     public static final int NOT_DELETED = 0;
-    public static final long SYSTEM_USER_ID = 0L;
+    public static final String SYSTEM_USER_ID = "00000000000000000000000000000000";
 
     private DataFlag() {
     }
