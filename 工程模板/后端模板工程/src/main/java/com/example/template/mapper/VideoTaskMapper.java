bd34238810852a7bfdcbb55798acef339dc89b5f
之道工作室 <126126815+zhidao-studio@users.noreply.github.com>
2026-07-07 12:25:57 +0800

    新增视频任务 Mapper

---
+++ 工程模板/后端模板工程/src/main/java/com/example/template/mapper/VideoTaskMapper.java
@@ -0,0 +1,9 @@
+package com.example.template.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.template.domain.video.entity.VideoTask;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface VideoTaskMapper extends BaseMapper<VideoTask> {
+}
